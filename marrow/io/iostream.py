78e220d3707ec90fec6ccba49ec8a8980dd1c84a
Alice Bevan-McGregor <alice.mcgregor@me.com>
2011-01-13 21:58:29 -0800

    Updated copyright statements.

--- marrow/io/iostream.py
+++ marrow/io/iostream.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 #
 # Copyright 2009 Facebook
-# Copyright 2010 Alice Bevan-McGregor
+# Portions Copyright 2010-2011 Alice Bevan-McGregor
 #
 # Licensed under the Apache License, Version 2.0 (the "License"); you may
 # not use this file except in compliance with the License. You may obtain
