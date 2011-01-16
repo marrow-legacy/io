27b47694e3ed7cfacf0d1e5e05d2323f60f8d2b7
Alice Bevan-McGregor <alice.mcgregor@me.com>
2011-01-15 22:32:15 -0800

    Updated raw example (connection overhead, rsecs) and added a raw-throughput example (mb/s).

--- examples/raw-throughput.py
+++ examples/raw-throughput.py
@@ -4,13 +4,26 @@
 
 from marrow.io.reactor import Reactor
 
+try:
+    range = xrange
+except:
+    pass
+
 
 reactor = Reactor()
+chunk = b"a" * 4096
+
 
 @reactor.inline_callbacks
 def serve(stream):
-    yield stream.write(b"HTTP/1.0 200 OK\r\nContent-Length: 7\r\n\r\nPong!\r\n")
+    yield stream.write(b"HTTP/1.0 200 OK\r\nContent-Length: 4194304\r\n\r\n")
+    
+    # Send 4MiB of data, 4KiB at a time.
+    for i in range(1024):
+        yield stream.write(chunk)
+    
     stream.close()
+
 
 reactor.listen(('127.0.0.1', 8010), 1024, serve)
 reactor.start()
