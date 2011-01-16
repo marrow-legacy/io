3f03bdc89dd32ace7b0b09f6925a8f60bdbb22be
Alice Bevan-McGregor <alice.mcgregor@me.com>
2011-01-15 21:48:02 -0800

    Initial rewrite of the raw HTTP example.

--- examples/raw.py
+++ examples/raw.py
@@ -1,53 +1,16 @@
 # encoding: utf-8
 
-"""An example raw IOLoop/IOStream example.
+"""An example raw marrow.io reactor example."""
 
-Taken from http://nichol.as/asynchronous-servers-in-python by Nicholas Piël.
-"""
-
-import errno
-import functools
-import socket
-
-from marrow.util.compat import exception
-from marrow.io import ioloop, iostream
-
-log = __import__('logging').getLogger(__name__)
+from marrow.io.reactor import Reactor
 
 
-def connection_ready(sock, fd, events):
-    while True:
-        try:
-            connection, address = sock.accept()
-        
-        except socket.error:
-            exc = exception().exception
-            if exc.args[0] not in (errno.EWOULDBLOCK, errno.EAGAIN):
-                raise
-            
-            return
-        
-        connection.setblocking(0)
-        stream = iostream.IOStream(connection)
-        stream.write(b"HTTP/1.0 200 OK\r\nContent-Length: 5\r\n\r\nPong!\r\n", stream.close)
+reactor = Reactor()
 
-if __name__ == '__main__':
-    import logging
-    
-    logging.basicConfig(level=logging.DEBUG)
-    
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
-    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    sock.setblocking(0)
-    sock.bind(("", 8010))
-    sock.listen(5000)
-    
-    io_loop = ioloop.IOLoop.instance()
-    io_loop.set_blocking_log_threshold(2)
-    callback = functools.partial(connection_ready, sock)
-    io_loop.add_handler(sock.fileno(), callback, io_loop.READ)
-    try:
-        io_loop.start()
-    except KeyboardInterrupt:
-        io_loop.stop()
-        log.info("exited cleanly")
+@reactor.inline_callbacks
+def serve(stream):
+    yield stream.write(b"HTTP/1.0 200 OK\r\nContent-Length: 7\r\n\r\nPong!\r\n")
+    stream.close()
+
+reactor.listen(('127.0.0.1', 8010), 5, serve)
+reactor.start()
